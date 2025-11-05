097b93637cd94a97fb5d7ede32c3bab55631f2a6
Morej <jeromeaw02@gmail.com>
2025-11-06 00:42:17 +0800

    create browser Router and landing page file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 export default function LandingPage() {
   return (
-    <div style={{ backgroundColor: "red", height: "100vh" }}>
+    <div style={{ backgroundColor: "red", height: "100vh", width: "100vw" }}>
       Landing Page
     </div>
   )
